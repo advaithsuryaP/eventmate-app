c85313b3010ae9797a3f49acd7ef0be44a19b591
advaithsuryaP <padvaithsurya@gmail.com>
2026-05-03 17:47:29 -0400

    Add planning-service application with initial configuration and setup
    - Introduced new planning-service application including essential configuration files such as project.json, tsconfig files, and webpack configuration.
    - Added launch configuration for debugging the planning-service in VSCode.
    - Implemented basic structure for the planning-service with main entry point, controller, service, and module files.
    - Included an asset directory for future use.

--- apps/planning-service/webpack.config.js
+++ apps/planning-service/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
     output: {
-        path: join(__dirname, '../../dist/apps/event-service'),
+        path: join(__dirname, '../../dist/apps/planning-service'),
         clean: true,
         ...(process.env.NODE_ENV !== 'production' && {
             devtoolModuleFilenameTemplate: '[absolute-resource-path]'
